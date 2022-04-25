afc9674d877251022122b7b51dda1b06567a1b57
Mikaella Mae Silva <mikaellamaecsilva@gmail.com>
2022-04-25 13:13:50 +0800

    save all

---
+++ PDC03_MOD02/PDC03_MOD02/activity2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity2 : ContentPage
+    {
+        public activity2()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged (object sender, ValueChangedEventArgs args)
+        {
+            var value = args.NewValue.ToString("0");
+            valueLabel.Text = String.Format("Rate how much you like Xamarin : {0}", value);
+
+        }
+
+
+         async void OnButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Xamarin Trivia", "Did you know that Xamarin was named after tamarin monkeys? So if you ever wondered, why the mascot of Xamarin is a monkey, here is your answer!", "Now, I know");
+        }
+
+    }
+}
